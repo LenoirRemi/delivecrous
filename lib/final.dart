bf96a229b5dbaf1e893594ee511aafb12d738e51
LenoirRemi <biaremi@hotmail.fr>
2020-04-23 08:18:44 +0200

    scrollView

--- lib/final.dart
+++ lib/final.dart
@@ -7,33 +7,35 @@ class Final extends StatelessWidget {
       appBar: AppBar(
         title: Text("Commande envoyée"),
       ),
-      body: Container(
+      body: SingleChildScrollView(
+        child: Container(
         margin: const EdgeInsets.all(12.0),
-        child: Column(
-          children: <Widget>[
-            Text("Votre commande a bien été enregistrée.",
-              style: TextStyle(
-                height: 2,
-                fontSize: 20,
+          child: Column(
+            children: <Widget>[
+              Text("Votre commande a bien été enregistrée.",
+                style: TextStyle(
+                  height: 2,
+                  fontSize: 20,
+                ),
               ),
-            ),
-            Image.asset(
-              'assets/images/delivery1.gif',
-              height: 345,
-            ),
-            Text("Nos livreurs s'occupent du reste !",
-              style: TextStyle(
-                height: 1.5,
-                fontSize: 20,
+              Image.asset(
+                'assets/images/delivery1.gif',
+                height: 345,
               ),
-            ),
-            Image.asset(
-              'assets/images/delivery2.gif',
-              width: 200,
-              height: 160,
-            ),
-          ],
-        )
+              Text("Nos livreurs s'occupent du reste !",
+                style: TextStyle(
+                  height: 1.5,
+                  fontSize: 20,
+                ),
+              ),
+              Image.asset(
+                'assets/images/delivery2.gif',
+                width: 200,
+                height: 160,
+              ),
+            ],
+          )
+        ),
       )
     );
   }
