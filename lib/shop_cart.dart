026b9eda7ae95c80620cc9e241d002cb2f988737
LenoirRemi <biaremi@hotmail.fr>
2020-04-27 11:01:40 +0200

    layout

--- lib/shop_cart.dart
+++ lib/shop_cart.dart
@@ -22,128 +22,131 @@ class ShopCart extends StatelessWidget {
             tileMode: TileMode.clamp
           )
         ),
-        child: Container(
-          margin: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Container(
-                child: Row(
-                  children: [
-                  Text("Panier",
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Panier",
                     style: TextStyle(
                       fontSize: 24,
                     ),
                   ),
-                  ],
                 ),
-              ),
-              Container(
-                height: 250,
+                Container(
+                  height: 250,
                   child: ListView.separated(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: 3,
-                  itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        height: 50,
-                        child: Center(child: Text(AppCats.cats[index]["name"])),
-                      );
-                  },
-                  separatorBuilder: (BuildContext context, int index) => const Divider(),
+                    padding: const EdgeInsets.all(8),
+                    itemCount: 3,
+                    itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          height: 50,
+                          child: Center(child: Text(AppCats.cats[index]["name"])),
+                        );
+                    },
+                    separatorBuilder: (BuildContext context, int index) => const Divider(),
+                  ),
                 ),
-              ),
-              Container(
-                child: Row(
-                  children: [
-                  Text("Où veut-tu te faire livrer ?\nEn salle de TD?",
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text("Où veut-tu te faire livrer ?\nEn salle de TD?",
                     style: TextStyle(
                       fontSize: 20,
                     ),
-                  ),
+                  ),  
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          labelText: "Adresse",  
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(15.7),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.7),
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
-              ),
-              
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        labelText: "Adresse",  
-                        enabledBorder: UnderlineInputBorder(
+                SizedBox(
+                  height: 8.0,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "CP",
+                          enabledBorder: UnderlineInputBorder(
                           borderSide: BorderSide(color: Colors.white),
                           borderRadius: BorderRadius.circular(15.7),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.7),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "CP",
-                        enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(15.7),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.7),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.7),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "Ville",
-                        enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.white),
-                        borderRadius: BorderRadius.circular(15.7),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.7),
-                        ),
-                      ),
-                    ), 
-                  )
-                ],
-              ),
-              Container(
-                child: Center(
-                  child: RaisedButton(
-                    color: Colors.blueGrey,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => Final())
-                      );
-                    },
-                    child: 
-                      Text('Passez commande',
-                      style: TextStyle(
-                        color: Colors.white,
-                        )
-                      )
+                    SizedBox(
+                      width: 4.0,
                     ),
-                  ),
+                    Expanded(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "Ville",
+                          enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(15.7),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(15.7),
+                          ),
+                        ),
+                      ), 
+                    )
+                  ],
                 ),
-              ],
-          ),
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Center(
+                    child: RaisedButton(
+                      color: Colors.blueGrey,
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Final())
+                        );
+                      },
+                      child: 
+                        Text('Passez commande',
+                        style: TextStyle(
+                          color: Colors.white,
+                          )
+                        )
+                      ),
+                    ),
+                  ),
+                ],
+            ),
+            ),
           ),
         ),
       );
